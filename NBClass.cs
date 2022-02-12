0ad81fdf0935f7c579813bd21b8006479c142baa
cat32tw <mike1993xd@gmail.com>
2022-02-13 00:38:33 +0800

    存取子練習

---
+++ NBClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propery1
+{
+    internal class NBClass
+    {
+        private int price;
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
